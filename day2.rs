b5454d04da0e3046cbe8f1fcc34eec88a779762d
jozhear <110872058+jozhear@users.noreply.github.com>
2024-12-02 22:38:26 -0700

    Update day2.rs

--- day2.rs
+++ day2.rs
@@ -1,8 +1,15 @@
+// Not proud of this one lol.
+// Trying to keep up to one per day using a language I only learned a week ago, and kinda tough.
+// I mean, it works, my logic was sound... but there is 100% a better way of doing this.
+
+// Ah well, going to keep learning!!
+
 use std::fs;
 
 fn main() {
     let data = fs::read_to_string("day2.txt");
 
+    let mut badrows: Vec<Vec<i32>> = Vec::new();
     let mut allrows: Vec<Vec<i32>> = Vec::new();
     let mut rowints: Vec<i32>  = Vec::new();
     let mut i = 0;
@@ -19,15 +26,16 @@ fn main() {
     },
     Err(error) => eprintln!("Error reading file: {error}"),
     }
-    for row in allrows{
+    for row in &allrows{
         let mut sortedrow = row.clone();
         sortedrow.sort();
         if *row == sortedrow{
-            for entry in &row[0..row.len() - 1]{
+            for entry in &row[..row.len() - 1]{
                 if entry - &row[i+1] >= -3 && entry - &row[i+1] <= -1{
                     i+=1;
                 }
                 else {
+                    badrows.push(row.clone());
                     break;
                 }
                 if i + 1 == row.len(){
@@ -42,6 +50,7 @@ fn main() {
                     i+=1;
                 }
                 else {
+                    badrows.push(row.clone());
                     break;
                 }
                 if i + 1 == row.len(){
@@ -51,5 +60,50 @@ fn main() {
         }
         i = 0;
     }
-    println!{"{:?}",safereports}
+    let mut z = 0;
+    let mut newreports = 0;
+    for row in &allrows{
+        z = 0;
+        'outer: for report in row{
+            i = 0;
+            let mut duperow = row.clone();
+            let piece = duperow.remove(z);
+            let mut sortedrow = duperow.clone();
+            sortedrow.sort();
+            if *duperow == sortedrow{
+                'inner: for entry in &duperow[..duperow.len() - 1]{
+                    if entry - &duperow[i+1] >= -3 && entry - &duperow[i+1] <= -1{
+                        i+=1;
+                    }
+                    else {
+                        break 'inner;
+                    }
+                    if i + 1== duperow.len(){
+                        newreports+=1;
+                        break 'outer;
+                    }
+                }
+            }
+            sortedrow.reverse();
+            if *duperow == sortedrow{
+                'inner: for entry in &duperow[..duperow.len() - 1]{
+                    if entry - &duperow[i+1] <= 3 && entry - &duperow[i+1] >= 1{
+                        i+=1;
+                    }
+                    else {
+                        break 'inner;
+                    }
+                    if i + 1== duperow.len(){
+                        newreports+=1;
+                        break 'outer;
+                    }
+                }
+            }  
+            println!("{:?}",duperow);
+            println!("{:?}",i);
+            z+=1;
+        }      
+    }
+    println!{"{:?}",safereports};
+    println!{"{:?}",newreports};
 }
